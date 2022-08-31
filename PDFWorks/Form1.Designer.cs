e78285eb45046bfc912d322bf53694fbd1e4822d
Faruk Çakal <fcakal07@gmail.com>
2022-08-31 13:05:41 +0300

    pdf merge method created

--- PDFWorks/Form1.Designer.cs
+++ PDFWorks/Form1.Designer.cs
@@ -28,33 +28,93 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnFirstSelect = new System.Windows.Forms.Button();
+            this.btnSecondSelect = new System.Windows.Forms.Button();
+            this.btnMerge = new System.Windows.Forms.Button();
+            this.lblFirstFile = new System.Windows.Forms.Label();
+            this.lblSecondFile = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
             this.SuspendLayout();
             // 
-            // button1
+            // btnFirstSelect
             // 
-            this.button1.Location = new System.Drawing.Point(46, 24);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnFirstSelect.Location = new System.Drawing.Point(46, 61);
+            this.btnFirstSelect.Name = "btnFirstSelect";
+            this.btnFirstSelect.Size = new System.Drawing.Size(75, 23);
+            this.btnFirstSelect.TabIndex = 0;
+            this.btnFirstSelect.Text = "İlk PDF";
+            this.btnFirstSelect.UseVisualStyleBackColor = true;
+            this.btnFirstSelect.Click += new System.EventHandler(this.btnFirstSelect_Click);
+            // 
+            // btnSecondSelect
+            // 
+            this.btnSecondSelect.Location = new System.Drawing.Point(46, 90);
+            this.btnSecondSelect.Name = "btnSecondSelect";
+            this.btnSecondSelect.Size = new System.Drawing.Size(75, 23);
+            this.btnSecondSelect.TabIndex = 0;
+            this.btnSecondSelect.Text = "İkinci PDF";
+            this.btnSecondSelect.UseVisualStyleBackColor = true;
+            this.btnSecondSelect.Click += new System.EventHandler(this.btnSecondSelect_Click);
+            // 
+            // btnMerge
+            // 
+            this.btnMerge.Location = new System.Drawing.Point(46, 122);
+            this.btnMerge.Name = "btnMerge";
+            this.btnMerge.Size = new System.Drawing.Size(174, 23);
+            this.btnMerge.TabIndex = 0;
+            this.btnMerge.Text = "Birleştir";
+            this.btnMerge.UseVisualStyleBackColor = true;
+            this.btnMerge.Click += new System.EventHandler(this.btnMerge_Click);
+            // 
+            // lblFirstFile
+            // 
+            this.lblFirstFile.AutoSize = true;
+            this.lblFirstFile.Location = new System.Drawing.Point(161, 66);
+            this.lblFirstFile.Name = "lblFirstFile";
+            this.lblFirstFile.Size = new System.Drawing.Size(35, 13);
+            this.lblFirstFile.TabIndex = 1;
+            this.lblFirstFile.Text = "label1";
+            // 
+            // lblSecondFile
+            // 
+            this.lblSecondFile.AutoSize = true;
+            this.lblSecondFile.Location = new System.Drawing.Point(161, 95);
+            this.lblSecondFile.Name = "lblSecondFile";
+            this.lblSecondFile.Size = new System.Drawing.Size(35, 13);
+            this.lblSecondFile.TabIndex = 2;
+            this.lblSecondFile.Text = "label2";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblSecondFile);
+            this.Controls.Add(this.lblFirstFile);
+            this.Controls.Add(this.btnMerge);
+            this.Controls.Add(this.btnSecondSelect);
+            this.Controls.Add(this.btnFirstSelect);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnFirstSelect;
+        private System.Windows.Forms.Button btnSecondSelect;
+        private System.Windows.Forms.Button btnMerge;
+        private System.Windows.Forms.Label lblFirstFile;
+        private System.Windows.Forms.Label lblSecondFile;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
     }
 }
 
--- PDFWorks/Form1.cs
+++ PDFWorks/Form1.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using PdfSharp.Pdf.IO;
+using PdfSharp.Pdf;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,14 +9,57 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace PDFWorks
 {
     public partial class Form1 : Form
     {
+        string FirstFile;
+        string SecondFile;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnFirstSelect_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            FirstFile = openFileDialog1.FileName;
+            lblFirstFile.Text = FirstFile;
+        }
+
+        private void btnSecondSelect_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            SecondFile = openFileDialog1.FileName;
+            lblSecondFile.Text = SecondFile;
+        }
+
+        private void btnMerge_Click(object sender, EventArgs e)
+        {
+            string[] files = new string[2];
+            files[0] = FirstFile;
+            files[1] = SecondFile;
+            MergePDFs(files);
+        }
+        public void MergePDFs(params string[] pdfs)
+        {
+            using (PdfDocument targetDoc = new PdfDocument())
+            {
+                foreach (string pdf in pdfs)
+                {
+                    using (PdfDocument pdfDoc = PdfReader.Open(pdf, PdfDocumentOpenMode.Import))
+                    {
+                        for (int i = 0; i < pdfDoc.PageCount; i++)
+                        {
+                            targetDoc.AddPage(pdfDoc.Pages[i]);
+                        }
+                    }
+                }
+                saveFileDialog1.ShowDialog();
+                targetDoc.Save(saveFileDialog1.FileName);
+            }
         }
     }
 }
